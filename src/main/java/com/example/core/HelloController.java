78ae25a7c4f5e1432ad6483a4e1b2db5ff687869
jamesgmoconnell <jamesgmoconnell@gmail.com>
2016-02-15 21:13:05 +0000

    adding HelloController

---
+++ src/main/java/com/example/core/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.core;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello IT again!";
+	}
+	
+	@RequestMapping("/goodbye")
+		public String goodbye() {
+		return "Goodbye!";
+	}
+}
+
