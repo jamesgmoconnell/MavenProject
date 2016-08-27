f6e28e974d252b826c7d98c94b8e7156ca19c819
jamesgmoconnell <jamesgmoconnell@gmail.com>
2016-08-27 14:22:01 +0100

    clean up

--- src/main/resources/autotestresult_generator.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-grep return $1 | sed 's/^[\t]*return "//g' | sed 's:\\n";::g' | sed 's/\\n/\
-/g'> $2
-
-
--- src/main/resources/test_input_generator.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-grep @RequestMapping $1 | sed 's/^[ \t]*@RequestMapping("//g' | sed 's/")//g' > $2
-
-
